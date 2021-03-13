69c08bb45b53ed47f750762244523a5c661ea2ca
Brittany Javalera <brittjavalera@gmail.com>
2021-03-13 15:23:50 -0800

    added mongoose and created new people collection and new fruit documents with mongoose

--- app.js
+++ app.js
@@ -1,26 +1,57 @@
-const { MongoClient } = require("mongodb");
+//jshint esversion:6
+require('dotenv').config();
+const uri = process.env.DB_URI
 
-// Replace the uri string with your MongoDB deployment's connection string.
-const uri =
-  "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority";
+const mongoose = require('mongoose')
+mongoose.connect(uri, {useNewUrlParser: true}, { useUnifiedTopology: true })
 
-const client = new MongoClient(uri);
+const fruitSchema = new mongoose.Schema ({
+    name: String,
+    variety: String,
+    score: Number,
+    review: String,
+});
 
-async function run() {
-  try {
-    await client.connect();
+const Fruit = mongoose.model("fruit", fruitSchema);
 
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
+const fruit = new Fruit({
+    name: "Pear",
+    variety: "Bosch",
+    score: 7,
+    review: "It's ok"
+})
 
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
+// fruit.save()
 
-    console.log(movie);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+const personSchema = new mongoose.Schema({
+    name: String, 
+    age: Number
+})
+
+const Person = mongoose.model("Person", personSchema)
+
+const person = new Person({
+    name: "John",
+    age: 37
+})
+
+const kiwi = new Fruit({
+    name: "Kiwi",
+    score: 6,
+    review: "Too seedy"
+})
+
+const mango = new Fruit({
+    name: "Mango",
+    score: 10,
+    variety: "Manila",
+    review: "Perfect"
+})
+Fruit.insertMany([mango, kiwi], function(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("Successfully saved fruit")
+    }
+})
